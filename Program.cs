060f73beaf1091555ea6220ab3122dbd3dbc45b4
vahidselimov <100331397+vahidselimov@users.noreply.github.com>
2022-03-28 11:14:49 +0400

    task1

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _27._03._22.task1
+{
+    internal class Program
+    {
+        //task1:Verilmiş string variable-ın dəyərini içərisində hər söz arasında bir boşluq gələcək vəzyətə salan metod.
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+
+
+
+
+            string[] arr = sentence.Split(new char[] { ' ' });
+
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+
+
+
+
+            }
+
+
+            string sentence2 = string.Join(' ', arr);
+
+
+
+            Console.WriteLine(sentence2);
+        }
+    }
+}
